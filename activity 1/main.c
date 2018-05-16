db99eddb1de7028497567943507feb0c1a847b17
María Fernanda Magallanes <35668326+MaferMazu@users.noreply.github.com>
2018-05-16 11:23:48 -0400

    Update main.c

--- activity 1/main.c
+++ activity 1/main.c
@@ -13,42 +13,42 @@ int main(int argc, char *argv[]) {
 
 	while(true){
 
-		Console.WriteLine("Introduzca el nombre de la pelicula:");
-		String texto;
-		texto=Console.ReadLine();
+		printf("%s\n","Introduzca el nombre de la pelicula:");
+		char *texto;
+		scanf("%s", &texto);
 
 		if(texto.compare("---")==0){
 			while(true){
 				int num;
-				printf("%s\n","Configuracion de busqueda:\n\n
-				1. Longitud máxima del nombre de la película (por defecto, 1024 caracteres)\n
-				2. Longitud máxima del nombre de la canción (por defecto, 1024 caracteres)\n
-				3. Número máximo de canciones a imprimir (por defecto 16 canciones)\n
-				4. Eficiencia (por defecto 16 buckets)\n
-				5. Cancelar cambio de configuración\n
+				printf("%s\n","Configuracion de busqueda:\n\n\
+				1. Longitud máxima del nombre de la película (por defecto, 1024 caracteres)\n\
+				2. Longitud máxima del nombre de la canción (por defecto, 1024 caracteres)\n\
+				3. Número máximo de canciones a imprimir (por defecto 16 canciones)\n\
+				4. Eficiencia (por defecto 16 buckets)\n\
+				5. Cancelar cambio de configuración\n\
 				6. Salir del programa\n");
 
-				num=Console.ReadInt();
+				scanf("%d", &num);
 				if(0<num<7){
 					int num1;
 					if(num==1){
-						Console.WriteLine("Introduzca la longitud maxima del nombre de la pelicula");
-						num1=Console.ReadInt();
+						printf("%s\n","Introduzca la longitud maxima del nombre de la pelicula");
+						scanf("%d", &num1);
 						lenPelicula = num1;
 					}
 					if(num==2){
-						Console.WriteLine("Introduzca la longitud maxima del nombre de la cancion");
-						num1=Console.ReadInt();
+						printf("%s\n","Introduzca la longitud maxima del nombre de la cancion");
+						scanf("%d", &num1);
 						lenCancion = num1;
 					}
 					if(num==3){
-						Console.WriteLine("Introduzca la longitud maxima canciones a escribir");
-						num1=Console.ReadInt();
+						printf("%s\n","Introduzca la longitud maxima canciones a escribir");
+						scanf("%d", &num1);
 						lenPrintCancion = num1;
 					}
 					if(num==4){
-						Console.WriteLine("Introduzca la eficiencia");
-						num1=Console.ReadInt();
+						printf("%s\n","Introduzca la eficiencia");
+						scanf("%d", &num1);
 						eficiencia = num1;
 					}
 					if(num==5){
@@ -56,37 +56,57 @@ int main(int argc, char *argv[]) {
 						int lenCancion = 1024;
 						int lenPrintCancion = 16;
 						int eficiencia = 16;
-						Console.WriteLine("Se ha cancelado el cambio de configuracion y todos los valores estan por defecto");
+						printf("%s\n","Se ha cancelado el cambio de configuracion y todos los valores estan por defecto");
+						break;
 					}
 					if(num==6){
 						exit(0);
 					}
 				}
 			}
-			
-		}
-	
-	//Tamaño tabla
-	    HashTable_t *ht = ht_create(101);
 
-	    ht_insert(ht, "coco", "A Blessing and a Fessing");
-	    ht_insert(ht, "Avengers: Infinity War", "cancion y");
-	    ht_insert(ht, "Moana", "cancion j");
-	    ht_insert(ht, "coco", "Cave Dwelling on the Past");
-	    ht_insert(ht, "Avengers: Infinity War", "End Credits");
-	    ht_insert(ht, "coco", "Cave Dwelling on the Past");
-	    ht_insert(ht, "Avengers: Infinity War", "End Credits");
+		}
+
+	//Tabla
+			HashTable_t *ht = ht_create(eficiencia);
+			char linea[(lenPelicula+lenCancion+4)];
+			FILE *archivo;
+			archivo = fopen(argv[0],"r")
+			while(fgets(linea,(lenPelicula+lenCancion+4),(FILE*) archivo)){
+				char *token = strtok(linea,"\t");
+				int i=0;
+				while(token!=NULL){
+					if(i==0){
+						char cancion[lenCancion]=token;
+						token = strtok(NULL,"\t");
+						i=1;
+					}
+					if(i==1){
+						char pelicula[lenPelicula]=token;
+						token = strtok(NULL,"\t");
+						break;
+					}
+				}
+				ht_insert(ht, pelicula, cancion);
+			}
 
 
 		// printf table
 
 		//Esto retorna un linkedList
-	    List * lOne = ht_index(ht, "coco");
+			List * lReal = ht_index(ht, texto);
+
+			printf("-----Pelicula: %s-------\n",texto);
+
+			//imprimimos
+			display(lReal,lenPrintCancion);
+
+	    List * lOne = ht_index(ht, "Coco");
 
 	    printf("-----Pelicula coco-------");
 
 	    //imprimimos
-	    display(lOne,16);
+	    display(lOne,lenPrintCancion);
 
 	    //Esto retorna un linkedList
 	    List * lTwo = ht_index(ht, "Avengers: Infinity War");
@@ -94,7 +114,7 @@ int main(int argc, char *argv[]) {
 	    printf("-----Pelicula Avengers: Infinity War-------");
 
 	    //imprimimos
-	    display(lTwo,16);
+	    display(lTwo,lenPrintCancion);
 
 	    //Esto retorna un linkedList
 	    List * lTree = ht_index(ht, "Moana");
@@ -102,13 +122,11 @@ int main(int argc, char *argv[]) {
 	    printf("-----Pelicula Moana-------");
 
 	    //imprimimos
-	    display(lTree,16);
+	    display(lTree,lenPrintCancion);
 
 	    ht_free(ht);
 
 		system("pause");
 		return 0;
 	}
-	
 }
-
